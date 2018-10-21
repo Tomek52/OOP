671fbda29dc01d5add327d666514a0b38aab3f7a
t0m3k52 <tomek.szpytma@gmail.com>
2018-10-21 13:40:31 +0200

    solution

--- main.cpp
+++ main.cpp
@@ -1,1 +1,38 @@
-#include <iostream>
+#include<iostream> 
+using namespace std;    
+class A 
+{ 
+    public:   
+        A()  
+        { 
+            cout << "A's constructor called" << endl; 
+        }
+
+        virtual ~A() = default;
+};    
+
+class B : virtual public A
+{ 
+    public:   
+        B()  
+        { 
+            cout << "B's constructor called" << endl; 
+        } 
+};    
+
+class C: public B, virtual public A  
+{ 
+    public:   
+        C()  
+        { 
+            cout << "C's constructor called" << endl; 
+        } };   
+
+int main() 
+{     
+    A* a = new B; 
+    B* b = new C;     
+    delete a;
+    delete b;
+    return 0; 
+}
