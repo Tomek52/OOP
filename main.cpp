bf000169e90007aa752467cc7054a7ffd98d528c
Tomek <tomek.szpytma@gmail.com>
2018-10-21 13:38:17 +0200

    not working code

--- main.cpp
+++ main.cpp
@@ -1,1 +1,31 @@
-#include <iostream>
+#include<iostream> 
+using namespace std; 
+  
+class A 
+{ 
+public: 
+  A()  { cout << "A's constructor called" << endl; } 
+}; 
+  
+class B 
+{ 
+public: 
+  B()  { cout << "B's constructor called" << endl; } 
+}; 
+  
+class C: public B, public A 
+{ 
+public: 
+  C()  { cout << "C's constructor called" << endl; } 
+}; 
+
+
+  int main() 
+{ 
+    A* a = new B; 
+    C* a = new B;
+    B* b = new C;
+    return 0; 
+} 
+
+
